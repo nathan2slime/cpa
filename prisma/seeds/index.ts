358cdbd4f598bc3788c9e5366383eb561d77cc08
Jhonathan M. <nathan3boss@gmail.com>
2024-08-30 21:03:05 -0300

    fix(api): Rodar db:seed na buildtime (#14)

--- prisma/seeds/index.ts
+++ prisma/seeds/index.ts
@@ -3,12 +3,17 @@ import { hash } from 'bcrypt';
 
 (async () => {
   const prisma = new PrismaClient({ datasourceUrl: process.env.DATABASE_URL });
+  const login = process.env.ROOT_LOGIN || 'root';
+  const password = process.env.ROOT_PASSWORD || 'root';
+  const hasRoot = await prisma.user.findUnique({ where: { login } });
+
+  if (hasRoot) return;
 
   const user = await prisma.user.create({
     data: {
-      login: process.env.ROOT_LOGIN || 'root',
+      login,
       roles: [Role.ADMIN],
-      password: await hash(process.env.ROOT_PASSWORD || 'root', 10),
+      password: await hash(password, 10),
     },
   });
 })();
