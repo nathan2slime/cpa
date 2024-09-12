bdb5f9127d45aaf7ac8dee17e56d3a1a4da810ba
sandersonsamuel <sandersonsamuel401@gmail.com>
2024-09-12 14:35:06 -0300

    feat (web): sistema de paginação de forms completo

--- apps/web/src/app/forms/page.tsx
+++ apps/web/src/app/forms/page.tsx
@@ -4,41 +4,44 @@ import { Button } from '@/components/ui/button';
 import { useEffect, useState } from 'react';
 import { api } from '@/api';
 import { FormReq } from '@/types/form';
-import { Pen, Trash2 } from 'lucide-react';
-import { useRouter } from 'next/router';
+import { Trash2 } from 'lucide-react';
 
 import {
   Pagination,
   PaginationContent,
-  PaginationEllipsis,
   PaginationItem,
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination"
-import { useSearchParams } from 'next/navigation';
 
 
 function Forms() {
   // Simula os formulários criados
   const [forms, setForms] = useState<FormReq[]>();
 
+  //redireciona para o formulário
   const redirectToForm = (id: string) => {
     location.href = `/form/${id}`
   }
 
   const params: URLSearchParams = new URLSearchParams(window.location.search);
   const [page, setPage] = useState(params.get("page") || "1");
-  const [totalForms, setTotalForms] = useState<number>(1);
+  const [totalForms, setTotalForms] = useState<number>(0);
 
+  //limite por pagina de formularios q serão pegos
+  const perPage = 6
+
+  //quantidade de paginas totais arredondadas
+  const pagesRounded = Math.ceil(totalForms / perPage);
+
+  //muda o parametro page da url
   const setParams = (page: number) => {
     setPage(String(page));
     params.set("page", String(page));
 
     window.history.pushState(null, "", `?${params.toString()}`);
   };
-
-  console.log(totalForms);
 
   //criar um formulário e redireciona para o mesmo
   const createForm = async () => {
@@ -50,7 +53,7 @@ function Forms() {
 
   const getForms = async () => {
     try {
-      const res = await api.get<FormReq[]>(`/api/form/search?perPage=6`);
+      const res = await api.get<FormReq[]>(`/api/form/search?page=${page}&perPage=${perPage}`);
       setForms(res.data.data);
       setTotalForms(res.data.total);
     } catch (error) {
@@ -60,7 +63,7 @@ function Forms() {
 
   useEffect(() => {
     getForms()
-  }, []);
+  }, [page]);
 
   return (
     <>
@@ -80,7 +83,7 @@ function Forms() {
 
           <p className={'font-semibold mb-3'}>Formulários recentes</p>
 
-          <div className='border w-full rounded-xl p-2'>
+          <div className='border w-full rounded-xl'>
             {
               forms?.length === 0 && <p>Sem formulários criados, crie um formulario no botão acima "Criar novo formulário".</p>
             }
@@ -89,7 +92,7 @@ function Forms() {
                 <div>
                   {
                     forms.map(form => (
-                      <div key={form.id} onClick={()=> redirectToForm(form.id)} className={'flex justify-between items-center p-4 border-b last:border-none hover:bg-gray-100 cursor-pointer border-xl'}>
+                      <div key={form.id} onClick={()=> redirectToForm(form.id)} className={'flex justify-between items-center p-4 border-b last:border-none last:rounded-b-xl first:rounded-t-xl hover:bg-gray-100 cursor-pointer border-xl'}>
                         <div>
                           <p className={'font-semibold'}>{form.title}</p>
                           {/*<p className={'text-sm text-gray-500'}>Tempo de resposta: {form.time}</p>*/}
@@ -110,20 +113,19 @@ function Forms() {
         <Pagination>
           <PaginationContent>
             <PaginationItem>
-              <PaginationPrevious className={'cursor-pointer'} onClick={()=> page > 1 ? setParams( page -1 ) : null } />
+              <Button variant={'ghost'} disabled={Number(page) - 1 <= 0} className={'cursor-pointer disabled:text-gray-500 disabled:cursor-not-allowed'} onClick={()=> page > 1 ? setParams( page -1 ) : null }>Anterior</Button>
             </PaginationItem>
             <PaginationItem>
-              <PaginationLink onClick={()=> setParams(Number(page) - 2)}>{Number(page) - 2}</PaginationLink>
+              <Button variant={'ghost'} disabled={Number(page) - 1 <= 0} onClick={()=> Number(page) - 2 <= 0 ? setParams(Number(page) -1) : setParams(Number(page) - 2)}>{Number(page) - 2 < 1 ? 1 : Number(page) - 1}</Button>
             </PaginationItem>
             <PaginationItem>
-              <PaginationLink onClick={()=> setParams(Number(page) + 2)}>{Number(page) + 2}</PaginationLink>
+              <Button variant={'ghost'} disabled={Number(page) + 1 > pagesRounded && Number(page) + 2 > pagesRounded} onClick={()=> Number(page) + 2 > pagesRounded ? null : setParams((Number(page) + 2)) }>{Number(page) + 2}</Button>
             </PaginationItem>
             <PaginationItem>
-              <PaginationNext className={'cursor-pointer'} onClick={()=> setParams(Number(page) + 1)} />
+              <Button variant={'ghost'} disabled={Number(page) + 1 > pagesRounded} className={'cursor-pointer'} onClick={()=> Number(page) + 1 > pagesRounded ? null : setParams(Number(page) + 1)}>Próximo</Button>
             </PaginationItem>
           </PaginationContent>
         </Pagination>
-
 
       </main>
     </>
