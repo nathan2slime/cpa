3d7dcebb13d9b696e213837ba131091f598cdd5e
sandersonsamuel <sandersonsamuel401@gmail.com>
2024-09-13 16:31:13 -0300

    feat (web) página de edição do form integrada com o id

--- apps/web/src/app/form/[id]/page.tsx
+++ apps/web/src/app/form/[id]/page.tsx
@@ -1,31 +1,125 @@
 'use client';
-//import { Form } from '@/components/form';
-import CadasterForm from '@/components/CadasterForm/CadasterForm';
-import { useParams } from 'next/navigation';
-import { useState } from 'react';
 
-const FormPage = () => {
-  const params = useParams();
+import React, { useEffect, useState } from 'react';
+import { HeaderForm } from '../components/HeaderForm';
+import { NewFormQuestion } from '../components/cardForm';
+import { Input } from '@/components/ui/input';
+import { QuestionType } from '@/types/question';
+import { MenuOptionNewForm } from '../components/MenuOptionNewForm';
+import { Button } from '@/components/ui/button';
+import { PlusCircle, SquarePlus } from 'lucide-react';
 
-  if (params.id !== 'avaliacao-institucional') {
-    return <h1>&copy; ak 404</h1>;
-  }
+const NewForm: React.FC = () => {
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
+  const [title, setTitle] = useState<string>('');
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState<number | null>(null);
 
-  const [showQuestionnaire, setShowQuestionnaire] = useState(false);
-
-  const handleFormSubmitSuccess = () => {
-    setShowQuestionnaire(true);
+  const newQuestion: QuestionType = {
+    type: 'choose',
+    question: '',
+    options: [''],
   };
 
+  const addQuestion = () => {
+    setQuestions([...questions, newQuestion]);
+  };
+
+  const handleQuestionTitleChange = (e: string, index: number) => {
+    const updatedQuestions = [...questions];
+    if (updatedQuestions[index]) {
+      updatedQuestions[index].question = e;
+      setQuestions(updatedQuestions);
+    }
+  };
+
+  const removeOption = (questionIndex: number, optionIndex: number) => {
+    const updatedQuestions = [...questions];
+    if (updatedQuestions[questionIndex]) {
+      updatedQuestions[questionIndex].options.splice(optionIndex, 1);
+      setQuestions(updatedQuestions);
+    }
+  }
+
+  const removeQuestion = (index: number) => {
+    const updatedQuestions = [...questions];
+    if (updatedQuestions[index]) {
+      updatedQuestions.splice(index, 1);
+      setQuestions(updatedQuestions);
+    }
+    // Atualiza a questão ativa, caso a removida seja a ativa
+    if (activeQuestionIndex === index) {
+      setActiveQuestionIndex(null);
+    }
+  };
+
+  const handleOptionChange = (
+    e: string,
+    questionIndex: number,
+    optionIndex: number,
+  ) => {
+    const updatedQuestions = [...questions];
+    if (updatedQuestions[questionIndex]) {
+      updatedQuestions[questionIndex].options[optionIndex] = e;
+      setQuestions(updatedQuestions);
+    }
+  };
+
+  const handleAddOption = (index: number) => {
+    const updatedQuestions = [...questions];
+    updatedQuestions[index].options.push('');
+    setQuestions(updatedQuestions);
+  };
+
+  useEffect(() => {
+
+  }, []);
+
   return (
-    <div>
-      {/*<Form id="anual_avaliation" />*/}
-      {!showQuestionnaire && (
-        <CadasterForm onSubmitSuccess={handleFormSubmitSuccess} />
+    <main className="pt-5 w-full h-[90vh] custom-scrollbar overflow-y-auto">
+      <MenuOptionNewForm onClick={addQuestion} />
+
+      {questions.length === 0 && (
+        <div className="h-max w-full max-w-3xl m-auto p-3 bg-white rounded-lg flex justify-center">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="w-2/5"
+            onClick={addQuestion}
+          >
+            <p className="font-bold text-lg flex">Adicionar questão</p>
+            <PlusCircle className={'ml-2'} size={20} />
+          </Button>
+        </div>
       )}
-      {showQuestionnaire && <div />}
-    </div>
+
+      {questions.map((question, index) => (
+        <div key={index} onClick={() => setActiveQuestionIndex(index)}>
+          <NewFormQuestion
+            question={question}
+            index={index}
+            handleQuestionTitleChange={handleQuestionTitleChange}
+            handleOptionChange={handleOptionChange}
+            handleAddOption={handleAddOption}
+            isLastQuestion={index === questions.length - 1}
+            addQuestion={addQuestion}
+            removeQuestion={() => removeQuestion(index)}
+            removeOption={removeOption}
+            // Passa informações adicionais para saber se é a questão ativa
+            isActive={activeQuestionIndex === index}
+          />
+        </div>
+      ))}
+
+      {
+        questions.length > 0 && (
+          <div className={'flex w-full justify-center'}>
+            <Button>Finalizar Formulário</Button>
+          </div>
+        )
+      }
+
+    </main>
   );
 };
 
-export default FormPage;
+export default NewForm;
